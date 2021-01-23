e8970db451f001b81e74d0196975cd7032858435
alibek.gao <a.kazhibekov@mail.ru>
2021-01-23 12:49:35 +0200

    init: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = str.slice('');
+    const map = {};
+    let counter = 0;
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if(bracketsConfig[i][0] === bracketsConfig[i][1]) {
+            map[bracketsConfig[i][0]] = {type: 2,counter: []}
+        } else {
+            map[bracketsConfig[i][0]] = {type: 0,counter: []}
+            map[bracketsConfig[i][1]] = {type: 1,opener: bracketsConfig[i][0]}
+        }
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if(typeof map[arr[i]] === 'object') {
+            if (map[arr[i]].type === 2) {
+                if(map[arr[i]].counter.length > 0)  {
+                    if (map[arr[i]].counter.pop() !== counter-1) return false;
+                    counter--;
+                } else {
+                    map[arr[i]].counter.push(counter++);
+                }
+            }
+            if (map[arr[i]].type === 0) {
+                map[arr[i]].counter.push(counter++);
+            }
+            if (map[arr[i]].type === 1) {
+                if (map[map[arr[i]].opener].counter.pop() !== counter-1) return false;
+                else counter--;
+            }
+        }
+    }
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if(map[bracketsConfig[i][0]].counter.length > 0) return false;
+    }
+
+    return true;
+
 }
